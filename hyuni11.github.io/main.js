675f30db74a35128ae347a571256bea5b492df5d
JunHyeok <cret9711@naver.com>
2022-12-12 09:02:51 +0900

    commit

--- hyuni11.github.io/main.js
+++ hyuni11.github.io/main.js
@@ -5,12 +5,11 @@ document.addEventListener('DOMContentLoaded', function () {
     ctx.beginPath();
     ctx.moveTo(0, 60);
     ctx.lineTo(0,0);
-    ctx.lineTo(180,0);
-    ctx.lineTo(220,60);
+    ctx.lineTo(170,0);
+    ctx.lineTo(230,60);
     ctx.lineTo(400,60);
     
-    ctx.strokeStyle='#000';
-    // ctx.lineWidth = '2'
+    ctx.strokeStyle='--mg';
 
     ctx.stroke()
 })
